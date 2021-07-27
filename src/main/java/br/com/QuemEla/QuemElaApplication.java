5ceb02b16d6d48bdbbca63607c3112acfee5c06a
Francielly Santos <francielly.carvalho@aluno.ufabc.edu.br>
2021-07-27 15:16:39 -0300

    upando projeto inicializador

---
+++ src/main/java/br/com/QuemEla/QuemElaApplication.java
@@ -0,0 +1,13 @@
+package br.com.QuemEla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemElaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemElaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/QuemEla/QuemElaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.QuemEla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuemElaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
