ccabaad3b69214b88ec86ecf20118d7ab326964a
Francielly Santos <francielly.carvalho@aluno.ufabc.edu.br>
2021-07-27 17:01:51 -0300

    att home
    Resolvendo erro do retorno da página inicial

---
+++ src/main/java/br/com/QuemEla/view/Home.java
@@ -0,0 +1,16 @@
+package br.com.QuemEla.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Home {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+		return new ModelAndView("teste");
+	}
+}
