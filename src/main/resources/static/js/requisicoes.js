34f2bad93e93bc345d6f264fe03aed33c6403fc8
Francielly Santos <francielly.carvalho@aluno.ufabc.edu.br>
2021-07-29 22:17:30 -0300

    req

--- src/main/resources/static/js/requisicoes.js
+++ src/main/resources/static/js/requisicoes.js
@@ -2,42 +2,18 @@ $(document).ready(function() {
     $("#btn-login").click(function(){
         alert("aaaaa")
         $.ajax({
-            url : "http://localhost:9090/personagem/listar",
-            type : 'post',
-            data : {},
-            beforeSend : function(){
-                
-            }
+            url : "/personagem/listar",
+            method : "POST",
+            contentType : 'application/json',
+            dataType : 'json',
+            data : JSON.stringify(dadosForm)
+        })
+        .success(function(data){
+            alert(data);
+            alert(data.responseText);
         })
         .done(function(msg){
             
-        })
-        .fail(function(jqXHR, textStatus, msg){
-                alert(msg);
-        });
-    });
-
-    $("#btn-cadastro").click(function(){
-        alert("aaaaa")
-        $.ajax({
-            url : "http://localhost:9090/personagem/listar",
-            type : 'get',
-            contentType: 'application/json',
-            cache: false,
-            method: 'get',
-            dataType: 'json',
-            data: JSON.stringify({
-                nome : $("#nomeUsuarioCadastro").value,
-                senha : $("#senhaUsuarioCadastro").value,
-                foto:"aaaa",
-                data_cadastro:null
-            }),
-            beforeSend : function(){
-                
-            }
-        })
-        .done(function(msg){
-            alert(msg);
         })
         .fail(function(jqXHR, textStatus, msg){
             alert(msg);
